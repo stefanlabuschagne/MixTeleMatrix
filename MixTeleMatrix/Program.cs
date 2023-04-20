e21445c459de0392e827e9bda73e5c76eec06256
stefan <stefan.labuschagne@gmail.com>
2023-04-20 18:13:21 +0200

    Cleaned up comments

--- MixTeleMatrix/Program.cs
+++ MixTeleMatrix/Program.cs
@@ -9,22 +9,23 @@ namespace MixTeleMatrix
     {
         static void Main(string[] args)
         {
+            // Load Data
             var daVehicleList = Utils.LoadVehicleList();
-            // When we are here we have 2 Million Points!
-
 
             // Initalize the Quad Tree with the BaseArea
             // Calculate Quadrants
             // Load the vehicles into the Quadrants
             QuadTree QT = new QuadTree(daVehicleList);
 
+            // Get test Vehicles
             List<Vehicle> TestVehicles = Utils.GetTestVehcles();
 
-            // See if the test Vehicles are within the Range
-            //foreach (var TV in TestVehicles)
-            //{ 
-            //    Console.WriteLine(Utils.VehicleIsInBox(TV, new MixTeleMatrix.Rectangle(LatMin, LatMax, LonMin, LonMax)));
-            //}
+            // TODO!!!!
+            // Return the closest vehicle to each of the 10 TEST vehicles
+            foreach (var TV in TestVehicles)
+            {
+                QT.GetNearsestVehicle(TV);
+            }
 
             // Get the vehicle within the shortest distance from the TEST vehicle
 
