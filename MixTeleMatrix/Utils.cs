caaeb54d74d2fa3b2ff69c57cfc2838aef159742
stefan <stefan.labuschagne@gmail.com>
2023-04-26 10:36:45 +0200

    Neateded the Test Vehicle LIst

--- MixTeleMatrix/Utils.cs
+++ MixTeleMatrix/Utils.cs
@@ -83,68 +83,67 @@ namespace MixTeleMatrix
 
         return( new List<Vehicle>()
                 {
-    
                     new Vehicle()
-                {
-                    Positionid = 1,
-                                Latitude = (float)34.544909,
-                                Longitude = (float)-102.100843
-                            },
-                            new Vehicle()
-                {
-                    Positionid = 2,
-                                Latitude = (float)32.345544,
-                                Longitude = (float)-99.123124
-                            },
-                            new Vehicle()
-                {
-                    Positionid = 3,
-                                Latitude = (float)33.234235,
-                                Longitude = (float)-100.214124
-                            },
-                            new Vehicle()
-                {
-                    Positionid = 4,
-                                Latitude = (float)35.195739,
-                                Longitude = (float)-95.348899
-                            },
-                            new Vehicle()
-                {
-                    Positionid = 5,
-                                Latitude = (float)31.895839,
-                                Longitude = (float)-97.789573
-                            },
-                            new Vehicle()
-                {
-                    Positionid = 6,
-                                Latitude = (float)32.895839,
-                                Longitude = (float)-101.789573
-                            },
-                            new Vehicle()
-                {
-                    Positionid = 7,
-                                 Latitude = (float)34.115839,
-                                 Longitude = (float)-100.225732
-                             },
-                            new Vehicle()
-                {
-                    Positionid = 8,
-                                 Latitude = (float)32.335839,
-                                 Longitude = (float)-99.992232
-                             },
-                            new Vehicle()
-                {
-                    Positionid = 9,
-                                Latitude = (float)33.535339,
-                                Longitude = (float)-94.792232
-                            },
-                            new Vehicle()
-                {
-                    Positionid = 10,
-                                Latitude = (float)32.234235,
-                                Longitude = (float)-100.222222
-                            }
-                    });
+                    {
+                        Positionid = 1,
+                        Latitude = (float)34.544909,
+                        Longitude = (float)-102.100843
+                    },
+                    new Vehicle()
+                    {
+                         Positionid = 2,
+                        Latitude = (float)32.345544,
+                        Longitude = (float)-99.123124
+                    },
+                    new Vehicle()
+                    {
+                        Positionid = 3,
+                        Latitude = (float)33.234235,
+                        Longitude = (float)-100.214124
+                    },
+                    new Vehicle()
+                    {
+                        Positionid = 4,
+                        Latitude = (float)35.195739,
+                        Longitude = (float)-95.348899
+                    },
+                    new Vehicle()
+                    {
+                        Positionid = 5,
+                        Latitude = (float)31.895839,
+                        Longitude = (float)-97.789573
+                    },
+                    new Vehicle()
+                    {
+                        Positionid = 6,
+                        Latitude = (float)32.895839,
+                        Longitude = (float)-101.789573
+                    },
+                    new Vehicle()
+                    {
+                        Positionid = 7,
+                        Latitude = (float)34.115839,
+                        Longitude = (float)-100.225732
+                    },
+                    new Vehicle()
+                    {
+                        Positionid = 8,
+                        Latitude = (float)32.335839,
+                        Longitude = (float)-99.992232
+                    },
+                    new Vehicle()
+                    {
+                        Positionid = 9,
+                        Latitude = (float)33.535339,
+                        Longitude = (float)-94.792232
+                    },
+                    new Vehicle()
+                    {
+                        Positionid = 10,
+                        Latitude = (float)32.234235,
+                        Longitude = (float)-100.222222
+                    }
+                });
 
         }
 
@@ -186,12 +185,9 @@ namespace MixTeleMatrix
                 }
                 catch (Exception exception)
                 {
-
                 }
                 finally
                 {
-
-
                 }
 
             }
